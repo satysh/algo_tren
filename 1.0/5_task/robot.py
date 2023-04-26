8e5044cf339b82109e7bb5073d9fe40ac966f2d7
satysh <satyshevi@gmail.com>
2023-04-26 18:50:13 +0300

    add corr sol

--- 1.0/5_task/robot.py
+++ 1.0/5_task/robot.py
@@ -1,29 +1,42 @@
-from collections import deque
+k = int(input())
+operations = input()
 
+ans = 0
+curlen = 0
+for r in range(k, len(operations)):
+    if operations[r] == operations[r - k]:
+        curlen += 1
+        ans += curlen
+    else:
+        curlen = 0
+
+print(ans)
+
+
+
+'''
 k = int(input())
 operations = input()
 
 ans = 0
 l = 0
 r = k
-mem = deque(operations[l:r], k)
-for l in range(len(operations) - k):
+while r < len(operations):
     i = 0
-    j = r
     flag = False
-    while j < len(operations) and mem[i] == operations[j]:
+    while r < len(operations) and operations[l + i] == operations[r]:
         flag = True
-        j += 1
+        r += 1
         i += 1
-        if i == len(mem):
+        if i == k:
             i = 0
     if flag:
-        curlen = j - l
-        ans += curlen - k
+        curlen = r - l
+        ans += (curlen - k + 1) * (curlen - k) // 2
+    else:
+        r += 1
 
-    if r < len(operations):
-        mem.append(operations[r])
-    r += 1
-
+    l = r - k
 
 print(ans)
+'''
